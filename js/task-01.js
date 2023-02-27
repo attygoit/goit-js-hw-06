92072421aa71201184b003f08c5fc2513a9851eb
attygoit <attyemail@gmail.com>
2023-02-27 12:32:42 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,6 @@ const categoriesList = document.querySelector('#categories');
 const categoriesItems = document.querySelector('.item');
 console.log(`Number of categories: ${categoriesItems.length}`);
 categoriesItems.forEach(category => {
-    const categoryName = category.querySelector('h2').textContent;
-    const categoryElements = category.querySelectorAll('li').length;
-    console.log(`Category: ${categoryName}`);
-    console.log(`Elements: ${categoryElements}`);
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`);
 });
